d2fdf011b47963d4b59b4c2e7c0d46e63ae65d5e
Dhruv Gajera <dhruvgajera05@gmail.com>
2025-03-23 11:32:43 +0530

    Add swagger api documentation

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   // Initialize the app
@@ -12,6 +13,19 @@ async function bootstrap() {
 
   // Using validation pipe for input validation
   app.useGlobalPipes(new ValidationPipe());
+
+  // config for api documentation
+  const config = new DocumentBuilder()
+    .setTitle('Nest.js Project')
+    .setDescription('API documentation for out Nest.js application')
+    .setVersion('1.0')
+    .build();
+
+  // Create the document
+  const document = SwaggerModule.createDocument(app, config);
+
+  // Setup the api path
+  SwaggerModule.setup('api', app, document);
 
   // Start the server
   await app.listen(process.env.PORT ?? 3000);
