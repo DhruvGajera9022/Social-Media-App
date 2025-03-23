c39b1d011701c1ceb832cdb222647ced2bdfd0f6
Dhruv Gajera <dhruvgajera05@gmail.com>
2025-03-23 11:27:54 +0530

    Add some changes in main.ts file

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,16 @@ import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  // Initialize the app
   const app = await NestFactory.create(AppModule);
+
+  // Using custom logger instead of default logger
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
+
+  // Using validation pipe for input validation
   app.useGlobalPipes(new ValidationPipe());
+
+  // Start the server
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
