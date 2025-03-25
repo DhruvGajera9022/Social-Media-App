4a1f5e219a3b718108154cdd2b96d74c0a1f1b99
Dhruv Gajera <dhruvgajera05@gmail.com>
2025-03-25 16:21:25 +0530

    Add some changes

--- src/authentication/strategy/jwt.strategy.ts
+++ src/authentication/strategy/jwt.strategy.ts
@@ -14,6 +14,6 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   }
 
   async validate(payload: any) {
-    return { userId: payload.userId };
+    return { userId: payload.sub, role: payload.role };
   }
 }
