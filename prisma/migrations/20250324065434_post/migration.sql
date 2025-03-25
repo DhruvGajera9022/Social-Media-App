d15430cb9651316abe06105f9e606bb0789be293
Dhruv Gajera <dhruvgajera05@gmail.com>
2025-03-25 14:59:49 +0530

    Add roles dto and service

--- prisma/migrations/20250324065434_post/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - The `media_url` column on the `Posts` table would be dropped and recreated. This will lead to data loss if there is data in the column.
-
-*/
--- AlterTable
-ALTER TABLE "Posts" DROP COLUMN "media_url",
-ADD COLUMN     "media_url" TEXT[];
--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,7 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateRoleDto } from './dto/create-role.dto';
+import { UpdateUserDTO } from 'src/users/dto/update-user.dto';
 
 @Injectable()
 export class RolesService {
   constructor(private prisma: PrismaService) {}
+
+  // Create Role
+  async createRole(createRoleDto: CreateRoleDto) {
+    return this.prisma.roles.create({ data: createRoleDto });
+  }
+
+  // Get Roles
+  async findAll() {
+    return this.prisma.roles.findMany();
+  }
+
+  // Get Role By Id
+  async findOne(id: number) {
+    const role = await this.prisma.roles.findUnique({ where: { id } });
+    if (!role) {
+      throw new NotFoundException('Role not found');
+    }
+
+    return role;
+  }
+
+  // Edit Role
+  async EditRole(id: number, updateRoleDto: UpdateUserDTO) {
+    return this.prisma.roles
+      .update({
+        where: { id },
+        data: updateRoleDto,
+      })
+      .catch(() => {
+        throw new NotFoundException('Role not found');
+      });
+  }
+
+  // Delete Role
+  async deleteRole(id: number) {
+    return this.prisma.roles.delete({ where: { id } }).catch(() => {
+      throw new NotFoundException('Role not found');
+    });
+  }
 }
