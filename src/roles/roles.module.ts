e318fa02d78e8ba95cafe49dc1bb3532dff15631
Dhruv Gajera <dhruvgajera05@gmail.com>
2025-03-30 13:23:48 +0530

    Add some changes

--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { RolesController } from './roles.controller';
 import { RolesService } from './roles.service';
 import { RolesGuard } from './guard/roles.guard';
-
+// changes
 @Module({
   controllers: [RolesController],
   providers: [RolesService, RolesGuard],
