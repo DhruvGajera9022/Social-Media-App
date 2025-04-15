af1e70fe9aeacd4adeacc8739f44dd4968a4c40a
Dhruv Gajera <dhruvgajera05@gmail.com>
2025-04-15 16:41:00 +0530

    Add some changes

--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ProfileController } from './profile.controller';
 import { ProfileService } from './profile.service';
 import { PrismaService } from 'src/prisma/prisma.service';
-
+// changes
 @Module({
   controllers: [ProfileController],
   providers: [ProfileService, PrismaService],
