a78f9303a1991c32a882a89d1f963ce387478114
Dhruv Gajera <dhruvgajera05@gmail.com>
2025-04-17 14:53:51 +0530

    Add some changes in profile controller

--- src/utils/response.util.ts
+++ src/utils/response.util.ts
@@ -1,7 +1,27 @@
-export const Response = (status: boolean, message: string, data?: any) => {
-  return {
-    status,
-    message,
-    data,
-  };
+import { Response } from 'express';
+
+// export const Response = (status: boolean, message: string, data?: any) => {
+//   return {
+//     status,
+//     message,
+//     data,
+//   };
+// };
+
+// Success Response
+export const successResponse = (
+  res: Response,
+  message: string = 'Success',
+  data?: any,
+): void => {
+  res.status(200).json({ success: true, message, data });
+};
+
+// Error Response
+export const errorResponse = (
+  res: Response,
+  statusCode: number,
+  error: string,
+) => {
+  res.status(statusCode).json({ success: false, error });
 };
