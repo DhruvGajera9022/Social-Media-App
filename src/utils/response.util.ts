a6113eb0422a18ddd28fe2f0a2f935a157f1d223
Dhruv Gajera <dhruvgajera05@gmail.com>
2025-04-17 14:46:58 +0530

    Add some changes in post controller

--- src/utils/response.util.ts
+++ src/utils/response.util.ts
@@ -1,7 +1,19 @@
-export const Response = (status: boolean, message: string, data?: any) => {
-  return {
-    status,
-    message,
-    data,
-  };
+import { Response } from 'express';
+
+// Success Response
+export const successResponse = (
+  res: Response,
+  message: string = 'Success',
+  data?: any,
+): void => {
+  res.status(200).json({ success: true, message, data });
+};
+
+// Error Response
+export const errorResponse = (
+  res: Response,
+  statusCode: number,
+  error: string,
+) => {
+  res.status(statusCode).json({ success: false, error });
 };
