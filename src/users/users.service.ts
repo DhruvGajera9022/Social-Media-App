5cfb2f4c5f147cfe70a07600ab2fd01ac224f4eb
Dhruv Gajera <dhruvgajera05@gmail.com>
2025-04-22 12:17:15 +0530

    Add some changes in users service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UpdateUserDTO } from './dto/update-user.dto';
 
@@ -8,84 +12,104 @@ export class UsersService {
 
   // Get all users
   async users() {
-    return this.prisma.users.findMany({ omit: { password: true } });
+    try {
+      return this.prisma.users.findMany({ omit: { password: true } });
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
   }
 
   // Get user by id
   async userById(userId: number) {
-    const user = await this.prisma.users.findUnique({
-      where: { id: userId },
-      omit: { password: true },
-      include: { posts: true },
-    });
-    if (!user) {
-      throw new NotFoundException('User not found');
-    }
+    try {
+      const user = await this.prisma.users.findUnique({
+        where: { id: userId },
+        omit: { password: true },
+        include: { posts: true },
+      });
+      if (!user) {
+        throw new NotFoundException('User not found');
+      }
 
-    return user;
+      return user;
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
   }
 
   // Search user
   async searchUser(username: string, page: number, limit: number) {
-    const skip = (page - 1) * limit;
+    try {
+      const skip = (page - 1) * limit;
 
-    const users = await this.prisma.users.findMany({
-      where: {
-        username: {
-          contains: username,
-          mode: 'insensitive',
+      const users = await this.prisma.users.findMany({
+        where: {
+          username: {
+            contains: username,
+            mode: 'insensitive',
+          },
         },
-      },
-      skip,
-      take: limit,
-      omit: { password: true },
-    });
+        skip,
+        take: limit,
+        omit: { password: true },
+      });
 
-    const totalUsers = await this.prisma.users.count({
-      where: {
-        username: {
-          contains: username,
-          mode: 'insensitive',
+      const totalUsers = await this.prisma.users.count({
+        where: {
+          username: {
+            contains: username,
+            mode: 'insensitive',
+          },
         },
-      },
-    });
+      });
 
-    return {
-      page,
-      limit,
-      totalUsers,
-      totalPages: Math.ceil(totalUsers / limit),
-      users,
-    };
+      return {
+        page,
+        limit,
+        totalUsers,
+        totalPages: Math.ceil(totalUsers / limit),
+        users,
+      };
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
   }
 
   // Update user
   async updateUser(userId: number, updateUserDto: UpdateUserDTO) {
-    const { username, email, roleId } = updateUserDto;
+    try {
+      const { username, email, roleId } = updateUserDto;
 
-    const user = await this.prisma.users.findUnique({
-      where: { id: userId },
-    });
-    if (!user) {
-      throw new NotFoundException('User not found');
+      const user = await this.prisma.users.findUnique({
+        where: { id: userId },
+      });
+      if (!user) {
+        throw new NotFoundException('User not found');
+      }
+
+      return await this.prisma.users.update({
+        where: {
+          id: userId,
+        },
+        data: {
+          username,
+          email,
+          roleId,
+        },
+      });
+    } catch (error) {
+      throw new InternalServerErrorException(error);
     }
-
-    return await this.prisma.users.update({
-      where: {
-        id: userId,
-      },
-      data: {
-        username,
-        email,
-        roleId,
-      },
-    });
   }
 
   // Delete user
   async delete(id: number) {
-    return this.prisma.users.delete({
-      where: { id },
-    });
+    try {
+      return this.prisma.users.delete({
+        where: { id },
+      });
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
   }
 }
