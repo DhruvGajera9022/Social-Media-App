531b5c6d64db3364e7d9f2293fcabc477eb62754
Dhruv Gajera <dhruvgajera05@gmail.com>
2025-04-24 16:44:06 +0530

    Add follow notification

--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -4,9 +4,10 @@ import { ProfileService } from './profile.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { RedisOptions } from 'src/config/redis.config';
 import { CacheModule } from '@nestjs/cache-manager';
+import { NotificationsModule } from 'src/notifications/notifications.module';
 
 @Module({
-  imports: [CacheModule.registerAsync(RedisOptions)],
+  imports: [CacheModule.registerAsync(RedisOptions), NotificationsModule],
   controllers: [ProfileController],
   providers: [ProfileService, PrismaService],
   exports: [ProfileService],
